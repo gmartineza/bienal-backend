ad918f52bfc077e3947c1e254b18f90e58d3540c
GMGonzaloMata <gonza.mata2003@gmail.com>
2024-10-27 11:59:22 -0300

    Implementacion todas las funcionalidades crud y el filtro de búsqueda de eventos por fechas para la parte del administrador

--- ModuloGestion/ServicioGestionEventos/src/db/models/Event.js
+++ ModuloGestion/ServicioGestionEventos/src/db/models/Event.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const EventoSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  date: { type: Date, required: true },
+  date_inicio: { type: Date, required: true },
+  date_fin: { type: Date, required: true },
   location: { type: String, required: true },
   theme: { type: String },
   sculptors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Escultor' }],
-  sculptures: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Escultura' }],
   images: [{ type: String }], // Almacena URLs de imágenes
 }, { timestamps: true });
 
