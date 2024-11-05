eb76c64f65b05bde0889b8679ba67255b372b30e
IgnacioParra19 <ignacioparraingaramo@gmail.com>
2024-11-05 18:39:17 -0300

    eliminacion carpeta validacion

--- ModuloVotYVal/ServicioValidacion/src/api/models/sculpturModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const sculptureSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  totalVotes: { type: Number, default: 0 },
-  totalScore: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('Sculpture', sculptureSchema);
