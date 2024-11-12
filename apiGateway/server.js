c2c745a66e06df03d55cb3a9f0dd237c8fd361c5
Billones142 <stefanomerinoderui@gmail.com>
2024-11-12 12:27:12 -0300

    pruebas cambios dns

--- apiGateway/server.js
+++ apiGateway/server.js
@@ -1,8 +1,35 @@
 const { join } = require('path');
 const gateway = require('express-gateway');
-const renderSlap= require('./RenderSlap')
 
-renderSlap(10, 4) // intervals between 6 and 14 minutes
+
+switch (process.env.DEPLOY_ENV) {
+    case "render": // keeps the render services active
+        const renderSlap= require('./RenderSlap');
+        renderSlap(10, 4); // intervals between 6 and 14 minutes
+        break;
+    case "aws": // creates a server to verify the ssl certificate
+        /*const updateNoIpDns= require('./update_no-ip_dns');
+        const {getPublicIp, getLoadBalancerHostname}= require('./getPublicIp');
+        const updateDns= async () => updateNoIpDns(process.env.NOIP_USER,
+            process.env.NOIP_PASSWORD,
+            "bienal-backend.ddns.net",
+            await getLoadBalancerHostname('api-gateway','bienal-backend'))
+
+        updateDns()
+        setInterval(updateDns, // update NoIP dns record
+            1000*60*20 // 20 minutes
+        );*/
+
+        /*const express = require('express');
+        const app= express()
+        app.use('/.well-known/pki-validation', express.static(join(__dirname, '.well-known/pki-validation')));
+        app.listen(8080, () => {
+            console.log('Servidor de verificación de dominio en ejecución en el puerto 8080');
+          });*/
+        break;
+    default:
+        break;
+}
 
 gateway()
   .load(join(__dirname, 'config'))
