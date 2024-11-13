70afda5f6d2011d214d538b0303df1e10cf7efcb
LautaroGVega <vegalautaro177@gmail.com>
2024-11-12 22:33:17 -0300

    Nueva Ruta admin y scripts

--- ModuloVotYVal/ServicioAuth/src/api/routes/authRoutes.js
+++ ModuloVotYVal/ServicioAuth/src/api/routes/authRoutes.js
@@ -1,12 +1,13 @@
 // src/api/routes/authRoutes.js
 
 const express = require('express');
-const { verifyToken } = require('../middlewares/authMiddleware.js');
+const { verifyToken, verifyAdmin } = require('../middlewares/authMiddleware.js');
 const { authenticate } = require('../controller/authController.js');
 
 const router = express.Router();
 
 // Ruta de autenticación que verifica el token
 router.post('/authenticate', verifyToken, authenticate);
-
+// Ruta de autenticación que verifica el token SOLO PARA ADMINS
+router.post('/authenticateAdmin', verifyToken, verifyAdmin, authenticate);
 module.exports = router;
