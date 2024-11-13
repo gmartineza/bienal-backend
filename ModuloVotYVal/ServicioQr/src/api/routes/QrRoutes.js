14f3ae3443406d782c6990da7b1101ff03267cb3
LautaroGVega <vegalautaro177@gmail.com>
2024-11-13 15:39:07 -0300

    Qr funcionando

--- ModuloVotYVal/ServicioQr/src/api/routes/QrRoutes.js
+++ ModuloVotYVal/ServicioQr/src/api/routes/QrRoutes.js
@@ -1,36 +1,10 @@
+// src/api/routes/qrRoutes.js
 import express from 'express';
-import { generateToken, generateQRCode, validateQRToken, startTokenInterval } from '../controllers/qrController.js';
+import { generateQR, verifyToken } from '../controllers/qrController.js';
 
 const router = express.Router();
 
-// Iniciar el intervalo de regeneración de tokens al iniciar la aplicación
-startTokenInterval();
-
-// Ruta para generar el QR
-router.get('/generate', async (req, res) => {
-  const token = generateToken();
-  const eventNumber = req.query.eventNumber;
-  const sculptorNumber = req.query.sculptorNumber;
-
-  try {
-    const qrCodeData = await generateQRCode(token, eventNumber, sculptorNumber);
-    res.json({ qrCode: qrCodeData, token });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Ruta para validar el token QR
-router.get('/validate', async (req, res) => {
-  const { token } = req.query;
-
-  try {
-    const isValid = await validateQRToken(token);
-    res.json({ valid: isValid });
-  } catch (error) {
-    console.error('Error al validar el token:', error);
-    res.status(500).json({ valid: false });
-  }
-});
+router.get('/generate-qr/:numeroevento/:numeroescultor', generateQR);
+router.get('/:token/:numeroevento/:numeroescultor', verifyToken);
 
 export default router;
