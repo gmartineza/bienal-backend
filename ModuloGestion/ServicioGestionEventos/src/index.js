0f7eb728f2303fa2cf40dbf62ed3d8b14e826f0c
GuglielmoneSantiago <guglielmonesantiago69@gmail.com>
2024-11-14 00:49:53 -0300

    index eventos ruta bien

--- ModuloGestion/ServicioGestionEventos/src/index.js
+++ ModuloGestion/ServicioGestionEventos/src/index.js
@@ -16,7 +16,7 @@ const { errors } = require('celebrate');
 connectDB();
 
 app.use(cors({
-  origin: ['http://localhost:5173', process.env.FRONTEND_URL], // URL FRONTEND
+  origin: [process.env.FRONTEND_URL||'http://localhost:5173'], // URL FRONTEND
   credentials: true // Permite cookies y headers de autenticación
 }));
 
