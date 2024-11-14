13bac07a5cf22a2b604dae51ab43565316cf4e76
Billones142 <stefanomerinoderui@gmail.com>
2024-11-14 16:31:17 -0300

    cambio de nombre variable de entorno

--- ServicioBusqueda/src/config/db.js
+++ ServicioBusqueda/src/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
