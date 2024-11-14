c746aae4dd352ef580e20dc8768ed0c10d9c1cd0
IgnacioParra19 <ignacioparraingaramo@gmail.com>
2024-11-13 23:13:57 -0300

    cambios de userID y sculptorID de int a string

--- ModuloVotYVal/ServicioVotacion/src/api/validations/voteValidations.js
+++ ModuloVotYVal/ServicioVotacion/src/api/validations/voteValidations.js
@@ -3,8 +3,8 @@ const { body } = require('express-validator');
 
 const voteValidationRules = () => {
   return [
-    body('userId').isInt().withMessage('Invalid user ID'),
-    body('sculptorId').isInt().withMessage('Invalid sculptor ID'),
+    body('userId').isString().withMessage('Invalid user ID'),  // Changed to isString
+    body('sculptorId').isString().withMessage('Invalid sculptor ID'),  // Changed to isString
     body('eventId').isIn(['marmol', 'madera', 'piedra']).withMessage('Invalid event type'),
     body('score').isInt({ min: 1, max: 5 }).withMessage('Score must be between 1 and 5')
   ];
--- ModuloVotYVal/ServicioVotacion/src/db/models/voteModels.js
+++ ModuloVotYVal/ServicioVotacion/src/db/models/voteModels.js
@@ -1,29 +1,29 @@
-// src/db/models/voteModels.js
+
 const mongoose = require('mongoose');
 
 const voteSchema = new mongoose.Schema({
-  userId: { 
-    type: Number, 
-    required: true 
+  userId: {
+    type: String,  
+    required: true
   },
-  sculptorId: { 
-    type: Number, 
-    required: true 
+  sculptorId: {
+    type: String,  
+    required: true
   },
-  eventId: { 
-    type: String, 
-    required: true, 
-    enum: ["marmol", "madera", "piedra"] 
+  eventId: {
+    type: String,
+    required: true,
+    enum: ["marmol", "madera", "piedra"]
   },
-  score: { 
-    type: Number, 
-    min: 1, 
-    max: 5, 
-    required: true 
+  score: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
   },
-  createdAt: { 
-    type: Date, 
-    default: Date.now 
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 });
 
