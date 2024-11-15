e97be097c06aa7bbf33658a2f0afcdc8556bac1e
Billones142 <stefanomerinoderui@gmail.com>
2024-11-15 10:30:39 -0300

    added command to restart buscador deployment

--- docker/kubernetes/applyConfig.ps1
+++ docker/kubernetes/applyConfig.ps1
@@ -1,4 +1,4 @@
 kubectl apply (Get-ChildItem -Path .\docker\kubernetes\kubernetes_*.yml | ForEach-Object { "-f", $_.FullName })
-& kubectl rollout restart deployment api-gateway gestion-esculturas gestion-escultores gestion-eventos generador-qr registro-votos autorizacion
+& kubectl rollout restart deployment api-gateway gestion-esculturas gestion-escultores gestion-eventos generador-qr registro-votos autorizacion buscador
 & kubectl get deployments --namespace bienal-backend
 & kubectl get services --namespace bienal-backend -o wide
